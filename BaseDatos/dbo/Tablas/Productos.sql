7699eeeec200fddaedb840750d9ce55e8c76d592
kandy mora quesada <kmq6488@hotmail.com>
2021-07-19 16:48:59 -0600

    Update

--- BaseDatos/dbo/Tablas/Productos.sql
+++ BaseDatos/dbo/Tablas/Productos.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Productos]
 (
-	Categoria VARCHAR(250) NOT NULL PRIMARY KEY
- ,  Nombre VARCHAR(250) NOT NULL 
+	Categoria VARCHAR(250) NOT NULL 
+ ,  Nombre VARCHAR(250) NOT NULL PRIMARY KEY
  ,  Cantidad INT NOT NULL
  ,  Carateristica VARCHAR(250) NOT NULL
  ,  Precio INT NOT NULL
