01a430114296a13b8278129753f4c4c53c967832
kandy mora quesada <kmq6488@hotmail.com>
2021-07-19 17:12:16 -0600

    UPDATE

--- BaseDatos/dbo/Tablas/Productos.sql
+++ BaseDatos/dbo/Tablas/Productos.sql
@@ -3,7 +3,7 @@
 	Categoria VARCHAR(250) NOT NULL 
  ,  Nombre VARCHAR(250) NOT NULL PRIMARY KEY
  ,  Cantidad INT NOT NULL
- ,  Carateristica VARCHAR(250) NOT NULL
+ ,  Carateristicas VARCHAR(250) NOT NULL
  ,  Precio INT NOT NULL
  ,  Estado BIT NOT NULL
 )
